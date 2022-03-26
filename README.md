cbe714d209c2939e57845bb2b03e9fc99ed38305
03gibbss <sam-gibbs1@hotmail.co.uk>
2022-03-26 13:28:13 +0000

    Adding scss variables file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Creating a React Component Library to better learn react, styling in react and typescript.
 
 Following this tutorial: https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
+
+Additions from this tutorial: https://github.com/HarveyD/react-component-library (Adding scss variables and exporting them to make them available to projects consuming the library.)
--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import copy from "rollup-plugin-copy";
 
 import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -44,7 +45,18 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    plugins: [
+      dts(),
+      copy({
+        targets: [
+          {
+            src: "src/variables.scss",
+            dest: "dist",
+            rename: "variables.scss",
+          },
+        ],
+      }),
+    ],
     external: [/\.(css|less|scss)$/],
   },
 ];
